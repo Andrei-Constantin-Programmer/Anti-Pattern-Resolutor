332b7367a7ca99ad2ec92a697a9888f50e8b3e2f
maoqin <ucabm57@ucl.ac.uk>
2025-07-08 11:43:47 +0100

    Implement IBM Watson X LLM creation and connectivity tests; add test cases for Ollama and IBM LLMs.

--- AntiPattern_Remediator/src/core/llm_models/create_llm.py
+++ AntiPattern_Remediator/src/core/llm_models/create_llm.py
@@ -17,10 +17,31 @@ class LLMCreator:
     
     @staticmethod
     def _create_ibm(model_name: str, **kwargs):
-        try:
-            pass
-        except ImportError:
-            raise ImportError("")
+        from langchain_ibm import WatsonxLLM
+        import os
+        # from getpass import getpass
+        # watsonx_api_key = os.environ.get("WATSONX_APIKEY")
+        # if not watsonx_api_key:
+        #     watsonx_api_key = getpass("Watson X API Key: ")
+        #     os.environ["WATSONX_APIKEY"] = watsonx_api_key
+        watsonx_api_key = "DumEQrFTlzIBAS0NFBREVGqDpv0mdez8B861Z1zkB_7e"  
+        project_id = "0994b8ce-78cc-42ca-93fe-5112d16d0ec8"
+        os.environ["WATSONX_APIKEY"] = watsonx_api_key
+        parameters = {
+            "decoding_method": "sample",
+            "max_new_tokens": 100,
+            "min_new_tokens": 1,
+            "temperature": 0.5,
+            "top_k": 50,
+            "top_p": 1,
+        }
+        watsonx_llm = WatsonxLLM(
+            model_id="ibm/granite-3-3-8b-instruct",
+            url="https://us-south.ml.cloud.ibm.com",
+            project_id=project_id,
+            params=parameters,
+        )
+        return watsonx_llm
     
     @staticmethod
     def _create_vllm(model_name: str, **kwargs):
